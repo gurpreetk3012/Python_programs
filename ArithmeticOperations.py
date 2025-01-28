ba327f63fd79a1aef2969527a365026b2308fe0e
Gurpreet Kaur <155377816+gurpreetk3012@users.noreply.github.com>
2025-01-28 20:18:50 +0530

    added arithmetic operations into the repo

---
+++ ArithmeticOperations.py
@@ -0,0 +1,14 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Addition
+print(f"Sum = {num1+num2}")
+
+# Subtraction
+print(f"Difference = {num1-num2}")
+
+# Multiplication 
+print(f"Product = {num1*num2}")
+
+# Division
+print(f"Division = {num1/num2}")
