f28c306dc6c223588500854338d0105da31920c3
Gurpreet Kaur <155377816+gurpreetk3012@users.noreply.github.com>
2025-01-29 22:06:42 +0530

    added area of the rectangle

---
+++ RectangleArea.py
@@ -0,0 +1,8 @@
+# Rectangle area calculation
+
+length = float(input("Enter the length: "))
+width = float(input("Enter the width: "))
+
+area = length * width
+
+print(f"The area of {length} & {width} is: {area}cm²")
